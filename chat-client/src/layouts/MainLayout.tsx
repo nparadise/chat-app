99838831d3a790b70582e4f8a8d1791ceadb1319
nparadise <s_nakwon@naver.com>
2025-03-12 20:29:13 +0900

    add MainLayout component and update routing structure; enhance CSS for layout styling

---
+++ chat-client/src/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router";
+
+const MainLayout = () => {
+  return (
+    <>
+      <header className="w-dvw py-2 bg-blue-800 dark:bg-neutral-800 text-center text-white  text-xl font-bold">
+        실시간 채팅
+      </header>
+      <main className="w-dvw grow flex justify-center items-center">
+        <Outlet />
+      </main>
+      <footer>임시 푸터</footer>
+    </>
+  );
+};
+
+export default MainLayout;
--- chat-client/src/main.tsx
+++ chat-client/src/main.tsx
@@ -1,25 +1,28 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-import App from './App.tsx';
-import Chat from './components/Chat.tsx';
-import Login from './pages/Login.tsx';
-import Register from './pages/Register.tsx';
+import App from "./App.tsx";
+import Chat from "./components/Chat.tsx";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
 
-import './index.css';
+import "./index.css";
+import MainLayout from "./layouts/MainLayout.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
+        <Route element={<MainLayout />}>
+          <Route index element={<App />} />
 
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="/chat" element={<Chat />} />
+          <Route path="/chat" element={<Chat />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
