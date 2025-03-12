5a67a0da0178984670744e614e2b0d6e2cbbccbe
nparadise <s_nakwon@naver.com>
2025-03-13 01:47:45 +0900

    refactor MainLayout component; remove dark mode background from header and add dark mode styling to body

--- chat-client/src/layouts/MainLayout.tsx
+++ chat-client/src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 const MainLayout = () => {
   return (
     <>
-      <header className="w-dvw py-2 bg-blue-800 dark:bg-neutral-800 text-center text-white  text-xl font-bold">
+      <header className="w-dvw py-2 bg-blue-800 text-center text-white  text-xl font-bold">
         실시간 채팅
       </header>
       <main className="w-dvw grow flex justify-center items-center">
